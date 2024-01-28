db79a4a4a52e35085c36f5b057b4c45949b22901
seven7 <734633635@qq.com>
2024-01-28 21:53:41 +0800

    fix: wrong queue implementation in Queue.scala

--- playground/src/utils/Queue.scala
+++ playground/src/utils/Queue.scala
@@ -7,54 +7,57 @@ import miku._
 import miku.frontend._
 import miku.utils.util.uintToBitPat
 import miku.frontend.LA32Instructions._
-class CircularQueueInput[T <: Data](data: T) extends Bundle{
-    val op = UInt(2.W)  //op(1): valid, op(0): operation, 0 is enq, 1 is deq
-    val enq_data = UInt((data.getWidth).W)
-    val clear = Bool()
-
-    def enq_valid = op(1) & !op(0)
-    def deq_valid = op(1) & op(0)
+class CircularQueueInput[T <: Data](data: T) extends Bundle {
+    val clear     = Bool()
+    val enq_data  = UInt((data.getWidth).W)
+    val enq_valid = Bool()
+    val deq_valid = Bool()
 }
 
-class CircularQueueOutput[T <: Data](data: T) extends Bundle{
-    val top_data = UInt((data.getWidth).W)
-    val empty = Bool()
+class CircularQueueOutput[T <: Data](data: T) extends Bundle {
+    val front_data = UInt((data.getWidth).W)
+    val empty      = Bool()
 }
 
-class CircularQueue[T <: Data](element: T, size: Int) extends Module{
-    val io = IO(new Bundle{
-        val in = Input(new CircularQueueInput(element))
+// ? is a "full" signal needed?
+class CircularQueue[T <: Data](element: T, size: Int) extends Module {
+    val io = IO(new Bundle {
+        val in  = Input(new CircularQueueInput(element))
         val out = Output(new CircularQueueOutput(element))
     })
 
     def enqData(data: T): Unit = {
-        io.in.op := "b10".U
-        io.in.enq_data := data
+        io.in.enq_valid := true.B
+        io.in.enq_data  := data
     }
 
     def deqData: T = {
-        io.in.op := "b11".U
-        io.out.top_data.asTypeOf(element)    
+        io.in.deq_valid := true.B
+        io.out.front_data.asTypeOf(element)
     }
     val queue = RegInit(VecInit.fill(size)(0.U.asTypeOf(ValidIO(element))))
-    val rear = RegInit(0.U(log2Ceil(size).W))       //point to the end of queue
-    val rear_plus_one = rear + 1.U
-    val rear_minus_one = rear - 1.U
 
-    io.out.empty := !queue.map(q => q.valid).reduce(_ || _)
-    io.out.top_data := queue(rear).bits
-    when(io.in.clear){
-        for(i <- 0 until size){
+    val rear           = RegInit(0.U(log2Ceil(size).W)) // point to the end of queue
+    val front          = RegInit(0.U(log2Ceil(size).W))
+    val rear_plus_one  = rear + 1.U
+    val front_plus_one = front + 1.U
+
+    io.out.empty      := (rear === front)
+    io.out.front_data := queue(front).bits
+    when(io.in.clear) {
+        for (i <- 0 until size) {
             queue(i).valid := false.B
         }
     }
-    .elsewhen(io.in.enq_valid){
-        queue(rear).bits := io.in.enq_data
-        queue(rear).valid := true.B
-        rear := Mux(rear === (size-1).U, 0.U, rear_plus_one)
-    }
-    .elsewhen(io.in.deq_valid){
-        queue(rear).valid := false.B
-        rear := Mux(rear === 0.U, (size-1).U, rear_minus_one)
+    .otherwise {
+        when(io.in.enq_valid) {
+            queue(rear).bits  := io.in.enq_data
+            queue(rear).valid := true.B
+            rear              := Mux(rear === 0.U, (size - 1).U, rear_plus_one)
+        }
+        when(io.in.deq_valid) {
+            queue(front).valid := false.B
+            front              := Mux(front === (size - 1).U, 0.U, front_plus_one)
+        }
     }
 }
